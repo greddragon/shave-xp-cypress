e12cdbd5a137e087bf86b01e3e89720f8d7ef23a
Gerson dos Santos de Souza <gersonsouza97@outlook.com>
2023-04-02 17:26:57 -0300

    fazendo requisição para API

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,57 +2,49 @@
 
 import loginPage from "../support/pages/login";
 import shaversPage from "../support/pages/shavers";
+import data from "../fixtures/user-login.json";
 
 describe("login", () => {
   context("quando submeto o formulario", () => {
     it("deve logar com sucesso", () => {
-      const user = {
-        name: "Gerson",
-        email: "gerson@testes.com",
-        password: "testes123",
-      };
+      const user = data.sucess;
+
+      cy.request({
+        method: "POST",
+        url: "http://localhost:3000/users",
+        body: user,
+      }).then(function (res) {
+        expect(res.status).to.eq(201);
+      });
 
       loginPage.submit(user.email, user.password);
       shaversPage.header.userShoudBeLoggedIn(user.name);
     });
 
     it("não deve logar com senha incorreta", () => {
-      const user = {
-        name: "Gerson",
-        email: "gerson@testes.com",
-        password: "123456",
-      };
-
+      const user = data.invpass;
       loginPage.submit(user.email, user.password);
-
       const message =
         "Ocorreu um erro ao fazer login, verifique suas credenciais.";
       loginPage.noticeShouldBe(message);
     });
+
     it("não deve logar com email incorreto", () => {
-      const user = {
-        name: "Gerson",
-        email: "gerson@gmail.com",
-        password: "testes123",
-      };
-
+      const user = data.email404;
       loginPage.submit(user.email, user.password);
-
       const message =
         "Ocorreu um erro ao fazer login, verifique suas credenciais.";
       loginPage.noticeShouldBe(message);
     });
 
-    it.only("campos obrigatórios", () => {
+    it("campos obrigatórios", () => {
       loginPage.submit();
       loginPage.requiredFields("E-mail é obrigatório", "Senha é obrigatória");
     });
   });
 
   context("senha muito curta", () => {
-    const passwords = ["1", "12", "123", "1234", "12345"];
-
-    passwords.forEach((p) => {
+    data.shortpass.forEach((p) => {
       it(`não deve logar com a senha: ${p}`, () => {
         loginPage.submit("gerson@testes.com", p);
         loginPage.alertShouldBe("Pelo menos 6 caracteres");
@@ -61,18 +53,7 @@ describe("login", () => {
   });
 
   context("email formato incorreto", () => {
-    const emails = [
-      "gerson&gmail.com",
-      "gerson.com.br",
-      "@gmail.com",
-      "@",
-      "gerson@",
-      "1232212",
-      "@#!@###@!",
-      "xpto123",
-    ];
-
-    emails.forEach((e) => {
+    data.invemails.forEach((e) => {
       it(`não deve logar com a senha: ${e}`, () => {
         loginPage.submit(e, "testes123");
         loginPage.alertShouldBe("Informe um email válido");
